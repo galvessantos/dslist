67521f9788242c66a30578f5e67397a7ac3c27e8
galvessantos <gabrielww1@hotmail.com>
2025-01-20 18:37:26 -0300

    Project created

---
+++ src/main/java/com/gabriel/dslistboot/DslistbootApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.dslistboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DslistbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DslistbootApplication.class, args);
+	}
+
+}
