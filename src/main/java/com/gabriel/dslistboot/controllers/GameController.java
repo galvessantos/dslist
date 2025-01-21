5aa4747053619889841c4307c3266a7c3e3df230
galvessantos <gabrielww1@hotmail.com>
2025-01-21 18:27:53 -0300

    Game repository, service, controller

---
+++ src/main/java/com/gabriel/dslistboot/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.gabriel.dslistboot.controllers;
+
+import com.gabriel.dslistboot.dto.GameMinDTO;
+import com.gabriel.dslistboot.entities.Game;
+import com.gabriel.dslistboot.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping (value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
---
+++ src/main/java/com/gabriel/dslistboot/repositories/GameRepository.java
@@ -0,0 +1,8 @@
+package com.gabriel.dslistboot.repositories;
+
+import com.gabriel.dslistboot.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+
+}
